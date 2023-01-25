3b2fd2df686c3ed36f6809280bae11aceebc7125
Juan R. Gavilanes <jrgavilanes@gmail.com>
2023-01-25 22:29:33 +0100

    01-DataBinding

--- app/src/main/java/es/codekai/jrgeneratorandroid/MainActivity.kt
+++ app/src/main/java/es/codekai/jrgeneratorandroid/MainActivity.kt
@@ -2,10 +2,21 @@ package es.codekai.jrgeneratorandroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import es.codekai.jrgeneratorandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUI()
+    }
+
+    private fun setUI() {
+        binding.apply {
+            binding.person = Person(name = "juanra", age = 40)
+            textView.text = "hola nano"
+        }
     }
 }
