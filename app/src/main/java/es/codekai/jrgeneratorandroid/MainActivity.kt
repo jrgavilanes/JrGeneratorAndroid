59386edbee9f086f14c8766936d291302830aeba
Juan R. Gavilanes <jrgavilanes@gmail.com>
2023-01-25 19:56:20 +0100

    01-ViewBinding

--- app/src/main/java/es/codekai/jrgeneratorandroid/MainActivity.kt
+++ app/src/main/java/es/codekai/jrgeneratorandroid/MainActivity.kt
@@ -1,11 +1,30 @@
 package es.codekai.jrgeneratorandroid
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import es.codekai.jrgeneratorandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        showUI()
+    }
+
+    private fun showUI() {
+        binding.apply {
+            button.setOnClickListener {
+                if (progressBar.visibility == View.GONE && button.text == getString(R.string.show)) {
+                    progressBar.visibility = View.VISIBLE
+                    button.text = getString(R.string.hide)
+                } else {
+                    progressBar.visibility = View.GONE
+                    button.text = getString(R.string.show)
+                }
+            }
+        }
     }
 }
