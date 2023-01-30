3002ea8e46eca212553313eeb49f48d579130fbd
Juan Ramón Gavilanes <jrgavilanes@gmail.com>
2023-01-30 20:40:53 +0100

    06-RecyclerView

--- app/src/main/java/es/codekai/jrgeneratorandroid/MainActivity.kt
+++ app/src/main/java/es/codekai/jrgeneratorandroid/MainActivity.kt
@@ -16,12 +16,30 @@ class MainActivity : AppCompatActivity() {
 
     private fun setUI() {
         binding.apply {
+            val adapter = MyRecyclerAdapter()
             myRecycler.layoutManager = LinearLayoutManager(this@MainActivity)
+            myRecycler.adapter = adapter
+            adapter.submitList(terremotosProvider())
         }
     }
 
     private fun terremotosProvider(): List<Terremoto> {
         val terremotos = mutableListOf<Terremoto>()
+        terremotos.add(Terremoto("1", "Valencia", "5.4", 1234L, -380.2, 6.21))
+        terremotos.add(Terremoto("2", "Madrid", "6.4", 1235L, -280.2, 33.21))
+        terremotos.add(Terremoto("3", "Barcelona", "7.4", 1236L, -180.2, 23.21))
+        terremotos.add(Terremoto("4", "Sevilla", "8.4", 1237L, -480.2, 213.21))
+        terremotos.add(Terremoto("5", "León", "4.4", 1238L, -780.2, 23.2451))
+        terremotos.add(Terremoto("1", "Valencia", "5.4", 1234L, -380.2, 6.21))
+        terremotos.add(Terremoto("2", "Madrid", "6.4", 1235L, -280.2, 33.21))
+        terremotos.add(Terremoto("3", "Barcelona", "7.4", 1236L, -180.2, 23.21))
+        terremotos.add(Terremoto("4", "Sevilla", "8.4", 1237L, -480.2, 213.21))
+        terremotos.add(Terremoto("5", "León", "4.4", 1238L, -780.2, 23.2451))
+        terremotos.add(Terremoto("1", "Valencia", "5.4", 1234L, -380.2, 6.21))
+        terremotos.add(Terremoto("2", "Madrid", "6.4", 1235L, -280.2, 33.21))
+        terremotos.add(Terremoto("3", "Barcelona", "7.4", 1236L, -180.2, 23.21))
+        terremotos.add(Terremoto("4", "Sevilla", "8.4", 1237L, -480.2, 213.21))
+        terremotos.add(Terremoto("5", "León", "4.4", 1238L, -780.2, 23.2451))
         terremotos.add(Terremoto("1", "Valencia", "5.4", 1234L, -380.2, 6.21))
         terremotos.add(Terremoto("2", "Madrid", "6.4", 1235L, -280.2, 33.21))
         terremotos.add(Terremoto("3", "Barcelona", "7.4", 1236L, -180.2, 23.21))
--- app/src/main/java/es/codekai/jrgeneratorandroid/MyRecyclerAdapter.kt
+++ app/src/main/java/es/codekai/jrgeneratorandroid/MyRecyclerAdapter.kt
@@ -1,25 +1,42 @@
 package es.codekai.jrgeneratorandroid
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-class MyRecyclerAdapter : ListAdapter<Terremoto, MyRecyclerAdapter.MyRecyclerViewHolder>() {
+class MyRecyclerAdapter :
+    ListAdapter<Terremoto, MyRecyclerAdapter.MyRecyclerViewHolder>(DiffCallback) {
+
+    companion object DiffCallback : DiffUtil.ItemCallback<Terremoto>() {
+        override fun areItemsTheSame(oldItem: Terremoto, newItem: Terremoto): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Terremoto, newItem: Terremoto): Boolean {
+            return oldItem == newItem
+        }
+    }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): MyRecyclerAdapter.MyRecyclerViewHolder {
-        // todo captiulo 53. minuto 3:29
-        // https://www.udemy.com/course/android-completo-con-kotlin/learn/lecture/21491402#overview
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.my_list_item, parent, false)
+        return MyRecyclerViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: MyRecyclerAdapter.MyRecyclerViewHolder, position: Int){
-        // todo
+    override fun onBindViewHolder(holder: MyRecyclerAdapter.MyRecyclerViewHolder, position: Int) {
+        val terremoto = getItem(position)
+        holder.magnitude.text = terremoto.magnitud
+        holder.location.text = terremoto.lugar
     }
 
-    inner class MyRecyclerViewHolder(val view: View): RecyclerView.ViewHolder(view) {
-
+    inner class MyRecyclerViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val magnitude = view.findViewById<TextView>(R.id.txtMagnitude)
+        val location = view.findViewById<TextView>(R.id.txtLocation)
     }
 }
