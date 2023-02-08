792a59e73c8bf5da61408e623089963028451630
Juan R. Gavilanes <jrgavilanes@gmail.com>
2023-02-08 19:39:46 +0100

    12-Room-MVVM-Hilt-Aris/basic_mvvm

--- app/src/main/java/es/codekai/jrgeneratorandroid/MainActivity.kt
+++ app/src/main/java/es/codekai/jrgeneratorandroid/MainActivity.kt
@@ -1,11 +1,36 @@
 package es.codekai.jrgeneratorandroid
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import es.codekai.jrgeneratorandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: QuoteViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUI()
+        setObservers()
+    }
+
+    private fun setObservers() {
+        viewModel.quoteModel.observe(this) { quote ->
+            binding.apply {
+                tvAuthor.text = quote.author
+                tvQuote.text = quote.quote
+            }
+        }
+    }
+
+    private fun setUI() {
+        binding.apply {
+            viewContainer.setOnClickListener {
+                viewModel.randomQuote()
+            }
+        }
     }
 }
---
+++ app/src/main/java/es/codekai/jrgeneratorandroid/model/QuoteModel.kt
@@ -0,0 +1,3 @@
+package es.codekai.jrgeneratorandroid.model
+
+data class QuoteModel(val quote: String, val author: String)
